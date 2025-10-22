11680ce155cd85957014d424466ac60b8740e4a8
adimerpaul <adimer101@gmail.com>
2025-10-22 03:43:38 -0400

    funciaocnado datos de productore

--- front/src/pages/slims/SlimNuevoPenal.vue
+++ front/src/pages/slims/SlimNuevoPenal.vue
@@ -1,6 +1,6 @@
 <template>
-<!--  <SlimNuevo :showNumeroApoyoIntegral="false" :tipologias="tipologias"/>-->
   <CasoNuevo
+    v-if="ready"
     :showNumeroApoyoIntegral="false"
     :tipologias="tipologias"
     :titulo="'Registrar Nuevo Caso Penal'"
@@ -9,39 +9,46 @@
 </template>
 
 <script>
-// import SlimNuevo from 'pages/slims/SlimNuevo.vue'
-import CasoNuevo from "pages/casos/CasoNuevo.vue";
+import { nextTick } from 'vue'
+import CasoNuevo from 'pages/casos/CasoNuevo.vue'
 
 export default {
   name: 'SlimNuevoPenal',
-  components: {CasoNuevo },
+  components: { CasoNuevo },
   data () {
     return {
-      tipo: 'SLIM',
-      tipologias: [
-        'Violencia Física',
-        'Violencia Feminicida',
-        'Violencia Psicológica',
-        'Violencia Mediática',
-        'Violencia Simbólica y/o Encubierta',
-        'Violencia Contra la Dignidad, la Honra y el Nombre',
-        'Violencia Sexual',
-        'Violencia Contra los Derechos Reproductivos',
-        'Violencia en Servicios de Salud',
-        'Violencia Patrimonial y Económica',
-        'Violencia Laboral',
-        'Violencia en el Sistema Educativo Plurinacional',
-        'Violencia en el Ejercicio Político y de Liderazgo de la Mujer',
-        'Violencia Institucional',
-        'Violencia en la Familia',
-        'Violencia Contra los Derechos y la Libertad Sexual',
-        'Tipologias Multiples',
-        'Otra'
-      ],
+      ready: false,
+      tipo: null,
+      tipologias: []
     }
   },
-  mounted() {
-    console.log('Tipo de caso:', this.tipo);
+  async created () {
+    // 1) Asignas variables
+    this.tipo = 'SLIM'
+    this.tipologias = [
+      'Violencia Física',
+      'Violencia Feminicida',
+      'Violencia Psicológica',
+      'Violencia Mediática',
+      'Violencia Simbólica y/o Encubierta',
+      'Violencia Contra la Dignidad, la Honra y el Nombre',
+      'Violencia Sexual',
+      'Violencia Contra los Derechos Reproductivos',
+      'Violencia en Servicios de Salud',
+      'Violencia Patrimonial y Económica',
+      'Violencia Laboral',
+      'Violencia en el Sistema Educativo Plurinacional',
+      'Violencia en el Ejercicio Político y de Liderazgo de la Mujer',
+      'Violencia Institucional',
+      'Violencia en la Familia',
+      'Violencia Contra los Derechos y la Libertad Sexual',
+      'Tipologias Multiples',
+      'Otra'
+    ]
+
+    // 2) Esperas a que el DOM procese los cambios y luego habilitas el render
+    await nextTick()
+    this.ready = true
   }
 }
 </script>
