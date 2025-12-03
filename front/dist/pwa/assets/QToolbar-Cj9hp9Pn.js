9a24d2a5f80ef99db8317650966d54dba0da743d
adimerpaul <adimer101@gmail.com>
2025-12-03 06:15:50 -0400

    Creado version

---
+++ front/dist/pwa/assets/QToolbar-Cj9hp9Pn.js
@@ -0,0 +1,1 @@
+import{j as a,n as s,v as r,X as n}from"./index-Ck1IPAoR.js";const p=a({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:e}){const t=s(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>r("div",{class:t.value,role:"toolbar"},n(e.default))}});export{p as Q};
